d5e2194d7654a38729b7c341984b7aca2372fa48
TiboPaulJack <thibault.Garrigues@icloud.com>
2023-05-13 10:29:18 +0200

    Initial commit on separated repository Old commits are on the server repo

---
+++ src/assets/baseApi.js
@@ -0,0 +1,5 @@
+
+const baseApi = "https://projet-biblio-3d-production.up.railway.app/api"
+
+
+export default baseApi
---
+++ src/assets/baseHost.js
@@ -0,0 +1,5 @@
+
+
+const baseHost = 'http://localhost:3001/api';
+
+export default baseHost;
