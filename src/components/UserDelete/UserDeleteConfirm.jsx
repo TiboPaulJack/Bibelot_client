757de3fa1b98f18acb5a230c1084053fed78ee10
TiboPaulJack <thibault.Garrigues@icloud.com>
2023-05-17 10:33:54 +0200

    fixing bug on delete user

--- src/components/UserDelete/UserDeleteConfirm.jsx
+++ src/components/UserDelete/UserDeleteConfirm.jsx
@@ -6,7 +6,9 @@ import s3DeleteObject from "../../utils/S3DeleteObject.js";
 export default function UserDeleteConfirm({ setDeleteConfirm, userProducts, userData }) {
 
   const baseApi = import.meta.env.VITE_BASE_API
-  const { userId } = userData.id
+  const userId = userData.id
+  console.log(userId)
+  
 
   const deleteUserDataInS3 = async () => {
     for (const product of userProducts) {
@@ -32,6 +34,7 @@ export default function UserDeleteConfirm({ setDeleteConfirm, userProducts, user
       },
       }).then((res) =>
       {
+        // TODO: PROBLEME DE STATUT
         if (res.status === 203) {
           deleteUserDataInS3().then( () => {
             localStorage.removeItem( "token" )
